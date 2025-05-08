aaebab979e372622763e3b4e01080da0052de073
lugyll <lucas.gabdev@gmail.com>
2025-05-08 15:29:53 -0300

    Moved Path Integration

---
+++ test/integration/PurchaseTicker.test.ts
@@ -0,0 +1,32 @@
+import { GetTicket } from "../../src/application/usecase/GetTicket";
+import { PurchaseTicket } from "../../src/application/usecase/PurchaseTicket";
+import { PgPromiseAdapter } from "../../src/infra/database/DatabaseConnection";
+import { EventRepositoryDatabase } from "../../src/infra/repository/EventRepository";
+import { TicketRepositoryDatabase } from "../../src/infra/repository/TicketRepository";
+
+test("Deve comprar um ingresso para o evento", async function () {
+  // given
+  const connection = new PgPromiseAdapter();
+
+  const ticketRepository = new TicketRepositoryDatabase(connection);
+  const eventRepository = new EventRepositoryDatabase(connection);
+
+  const purchaseTicket = new PurchaseTicket(ticketRepository, eventRepository);
+  const getTicket = new GetTicket(connection);
+  
+  const inputPurchaseTicket = {
+    eventId: "185ff433-a7df-4dd6-ac86-44d219645cb1",
+    email: "john.doe@gmail.com"
+  };
+  // when
+  const outputPurchaseTicket = await purchaseTicket.execute(inputPurchaseTicket);
+  const outputGetTicket = await getTicket.execute(outputPurchaseTicket.ticketId);
+  
+  // then
+  expect(outputPurchaseTicket.ticketId).toBeDefined();
+  expect(outputGetTicket.eventId).toBe("185ff433-a7df-4dd6-ac86-44d219645cb1");
+  expect(outputGetTicket.email).toBe("john.doe@gmail.com");
+  expect(outputGetTicket.price).toBe(100);
+
+  await connection.close();
+})
