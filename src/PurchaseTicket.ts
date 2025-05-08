82a99cf92ae4a38a773c0514dc269e4f5ac0d9a7
lugyll <lucas.gabdev@gmail.com>
2025-05-08 12:04:35 -0300

    Create get event

--- src/PurchaseTicket.ts
+++ src/PurchaseTicket.ts
@@ -1,11 +1,18 @@
 //import crypto from "crypto";
 
+import pgp from "pg-promise";
+
 export class PurchaseTicket {
   constructor() { }
 
   async execute(input: Input): Promise<Output> {
     const ticketId = crypto.randomUUID();
+    const connection = pgp()("postgres:postgres:postgres@localhost:5432/cleanArquiteture");
+    const [event] = await connection.query("select * from lucas.event where event_id = $1", [input.eventId]);
 
+    await connection.query(`insert into lucas.ticket (ticket_id, event_id, email, price, status, date) values
+      ($1, $2, $3, $4, $5, $6)`, [ticketId, input.eventId, input.email, parseFloat(event.prince), "created", new Date()]);
+    await connection.$pool.end();
     return {
       ticketId
     }
