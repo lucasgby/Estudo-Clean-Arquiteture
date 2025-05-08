a7d93588a229b05c7310ef38ddcca19ae5de7891
lugyll <lucas.gabdev@gmail.com>
2025-05-08 11:28:26 -0300

    config: Tests Configuration

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  }
+};
